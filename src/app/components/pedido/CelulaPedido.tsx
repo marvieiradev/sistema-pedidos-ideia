55ff31c89792df71a0964b28a848d9c680d03c92
Marcos Vieira <marcosvieira482@gmail.com>
2024-07-31 18:14:31 -0300

    feat: update tables styles

--- src/app/components/pedido/CelulaPedido.tsx
+++ src/app/components/pedido/CelulaPedido.tsx
@@ -6,7 +6,7 @@ export interface CelulaPedidoProps {
 export default function CelulaPedido(props: CelulaPedidoProps) {
     return (
         <div className={`flex flex-col text-center text-txpry border-txquar p-[2px] ${props.class}`}>
-            <span className="text-[10px] md:text-sm lg:text-base font-semibold">
+            <span className="text-[10px] md:text-sm lg:text-base font-semibold overflow-hidden whitespace-nowrap">
                 {props.texto.toUpperCase()}
             </span>
         </div>
