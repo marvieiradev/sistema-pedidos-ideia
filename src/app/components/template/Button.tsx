36577d318eb9ace017501d782454742e7b3f581e
Marcos Vieira <marcosvieira482@gmail.com>
2024-08-01 08:07:13 -0300

    feat: update button style

--- src/app/components/template/Button.tsx
+++ src/app/components/template/Button.tsx
@@ -5,7 +5,7 @@ export interface ButtonProps {
 
 export default function Button(props: ButtonProps) {
     return (
-        <button className={`flex items-center px-3 py-2 rounded-md border-[2px] text-xs md:text-sm lg:text-base font-semibold uppercase ${props.class}`}>
+        <button className={`flex items-center justify-center px-3 py-2 rounded-md border-[2px] text-xs md:text-sm lg:text-base font-semibold uppercase ${props.class}`}>
             {props.texto}
         </button >
     )
--- src/app/components/template/Modal.tsx
+++ src/app/components/template/Modal.tsx
@@ -20,14 +20,14 @@ export default function Modal(props: ModalType) {
                             <div className="flex flex-col items-center px-5">
                                 <h1 className="mt-3 text-lg lg:mt-6 lg:text-xl text-center text-txpry font-bold uppercase">{props.text}</h1>
                                 <h3 className="text-sm text-center mt-3 lg:font-semibold lg:mt-10 text-vermelhosec uppercase">Verifique o pedido antes de confirmar!</h3>
-                                <div className="flex flex-row items-center justify-center gap-3 w-full mt-5 lg:mt-10 md:gap-4">
+                                <div className="flex flex-row items-center justify-center gap-3 w-full mt-5 lg:mt-10 md:gap-6">
                                     <div onClick={props.action}>
                                         <div onClick={props.toast}>
-                                            <Button texto="Sim" class="text-verdesec border-verdesec bg-verdepry" />
+                                            <Button texto="Sim" class="text-verdesec border-verdesec bg-verdepry w-[90px]" />
                                         </div>
                                     </div>
                                     <div onClick={props.toggle}>
-                                        <Button texto="Não" class="text-vermelhosec border-vermelhosec bg-vermelhopry" />
+                                        <Button texto="Não" class="text-vermelhosec border-vermelhosec bg-vermelhopry w-[90px]" />
                                     </div>
                                 </div>
                             </div>
